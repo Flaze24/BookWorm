4f92bf1dcb242abd8e04dda70f1b9eee093b3ddd
ElleryJose <elleryjose72@gmail.com>
2017-10-27 06:53:32 -0400

    Agregar busqueda por genero y crear pagina principal

--- js/search.js
+++ js/search.js
@@ -1,6 +1,7 @@
 const link=`https://www.googleapis.com/books/v1/volumes?q=`;
 const busBook=document.querySelector("#busBook");
 let resultado=document.querySelector("#result");
+const key="AIzaSyA7j7MlEFh0-LR0btN_w65bllOrpgfFx1Y";
 
 busBook.addEventListener('click', function(){
 		const sear=document.querySelector("#search").value;
@@ -9,21 +10,41 @@ busBook.addEventListener('click', function(){
 		resultado.innerHTML=""
 		console.log(search);
 		if(!search==""){
-			fetch(`${link}"${search}"&langRestrict=${leng}&maxResults=20&orderBy=newest`)
+			fetch(`${link}"${search}"&langRestrict=${leng}&maxResults=20&orderBy=relevance&key=${key}`)
 			.then((response)=>response.json())
 			.then((data)=>{
 				let book=data.items;
+				let unavailable="../img/unavailable.png"
 				console.log(book);
 				book.map((es)=>{
-					resultado.innerHTML += `<div class="col-xs-6 col-md-3>
-												<a href="#" class="thumbnail"><img src="${es.volumeInfo.imageLinks.thumbnail}" class="inmage foco image-responsive"></a>
+					// i=0
+					// if(i==0){
+					// 	resultado.innerHTML+=`<div class="row text-center">`
+					// }
+					resultado.innerHTML +=  `<div class="col-xs-6 col-md-3 genre>
+												<a href="#"><img src="${es.volumeInfo.imageLinks ? es.volumeInfo.imageLinks.thumbnail : unavailable }" class="foco image-responsive" style="width:130px; height:180px;"></a>
 												<br>
 												<strong>${es.volumeInfo.title}</strong>
-												<p>Autores: ${es.volumeInfo.authors}</p>
-												<a href="#" class="btn btn-primary">Revisar</a>
+												<p>Autores: ${es.volumeInfo.authors ? es.volumeInfo.authors : "Desconocido"}</p>
+												<a href="#" class="btn btn-primary revisar">Revisar</a>
 											</div>`
-					
-				});
+											// i++;
+											
+					// if(i==3){
+					// 	resultado.innerHTML+=`</div>`
+					// }
+								
+					console.log(resultado);
+
+					// let revisar=document.querySelectorAll(".revisar");
+					// revisar.addEventListener("click", function(e){
+					// 	e.preventDefault();
+					// 	let envio=revisar.value;
+					// 	console.log(envio);
+					// })
+										
+				});							
+
 			})
 			.catch((error)=>{
 					console.log(error);
